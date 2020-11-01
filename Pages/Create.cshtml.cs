62c3de89aaf94270ab8d0c7ffb95d46219300b52
kjrgreen <kevin.j.r.green@gmail.com>
2020-11-01 22:44:10 +0100

    Add support for uploading image when creating article

--- Pages/Create.cshtml.cs
+++ Pages/Create.cshtml.cs
@@ -7,6 +7,9 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using article_CMS.Data;
 using article_CMS.Models;
+using Microsoft.AspNetCore.Http;
+using System.Net.Mime;
+using System.IO;
 
 namespace article_CMS.Pages
 {
@@ -26,6 +29,8 @@ namespace article_CMS.Pages
 
         [BindProperty]
         public Article Article { get; set; }
+        [BindProperty]
+        public IFormFile Upload { get; set; }
 
         // To protect from overposting attacks, enable the specific properties you want to bind to, for
         // more details, see https://aka.ms/RazorPagesCRUD.
@@ -36,10 +41,18 @@ namespace article_CMS.Pages
                 return Page();
             }
 
+            if (Upload == null) return Page(); //Guard clause; don't do anything unless an image is included
+            if (Upload.ContentType != "image/jpeg") return Page(); //Don't allow users to upload files that are not jpegs. I'm not certain this is sufficient validation.
 
             Article.Created = DateTime.UtcNow;
             _context.Articles.Add(Article);
             await _context.SaveChangesAsync();
+
+            var file = Path.Combine("Uploads", Article.Id.ToString() + ".jpg");
+            using (var fileStream = new FileStream(file, FileMode.Create))
+            {
+                await Upload.CopyToAsync(fileStream);
+            }
 
             return RedirectToPage("./Index");
         }

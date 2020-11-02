784110a2af80652369cce3b0aad229289f39bab2
kjrgreen <kevin.j.r.green@gmail.com>
2020-11-02 02:07:09 +0100

    Add webservice which serves content While this will need one more feature, the CMS is all but done now. The remaining feature is to exclusively provide the datetime at which the latest update/creation was made, so that there is something to compare the clients cached content to. Also, there's still the issue with overposting.

---
+++ API/ArticlesController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using article_CMS.Data;
+using article_CMS.Models;
+using Microsoft.AspNetCore.Authorization;
+
+namespace article_CMS.API
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ArticlesController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ArticlesController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Articles
+        [AllowAnonymous]
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Article>>> GetArticles()
+        {
+            return await _context.Articles.ToListAsync();
+        }
+    }
+}
